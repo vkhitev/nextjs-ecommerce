89b7781a4a3edcbd7d770825f9c940b8e3d42dfc
Vlad Khitev <vkhitev@axon.dev>
2023-09-19 01:18:11 +0300

    feat: redirect to store upon creation

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,17 @@
-export default function DashboardPage() {
-  return <div>This is a dashboard</div>
+import prismadb from '@/lib/prismadb'
+
+interface DashboardPageProps {
+  params: {
+    storeId: string
+  }
+}
+
+export default async function DashboardPage({ params }: DashboardPageProps) {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  })
+
+  return <div>Active Store: {store?.name}</div>
 }
