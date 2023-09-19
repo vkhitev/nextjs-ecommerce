1d56b024e2cf391919a9d791d49bb6d183381689
Vlad Khitev <vkhitev@axon.dev>
2023-09-19 15:27:37 +0300

    feat: add store switcher popup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,27 @@
-'use client'
-
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth } from '@clerk/nextjs'
 
 import { MainNav } from '@/components/main-nav'
+import { StoreSwitcher } from '@/components/store-switcher'
+import { redirect } from 'next/navigation'
+import prismadb from '@/lib/prismadb'
 
-export function Navbar() {
+export async function Navbar() {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect('/sign-in')
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  })
+
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>Switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
